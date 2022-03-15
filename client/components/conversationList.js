1566bfcabce23e9be6afae0d6e2417d13c1004ae
Loai AlFarran <loayei@gmail.com>
2022-03-15 15:25:57 -0700

    Icons

---
+++ client/components/conversationList.js
@@ -0,0 +1,9 @@
+import styles from '../styles/conversationList.module.css'
+import { useEffect, useState } from 'react'
+import Image from 'next/image'
+
+const conversationList = () => {
+  return <div>conversationList</div>
+}
+
+export default conversationList
